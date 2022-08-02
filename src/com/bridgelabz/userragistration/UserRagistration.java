81fa5b7bf25388efca2936a7454e48a3044e1bf6
sujit <sujitbokade2014@gmail.com>
2022-08-02 21:52:01 +0530

    [Sujit]isValid Last Name

--- src/com/bridgelabz/userragistration/UserRagistration.java
+++ src/com/bridgelabz/userragistration/UserRagistration.java
@@ -8,6 +8,7 @@ public class UserRagistration {
     public static void main(String[] args) {
             UserRagistration userRegistration = new UserRagistration();
             userRegistration.validFirstName();
+            userRegistration.validLastName();
         }
         public void validFirstName(){
             Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
@@ -22,4 +23,17 @@ public class UserRagistration {
             else
                 System.out.println("First Name is InValid");
         }
+    public void validLastName(){
+        Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter User Last Name: ");
+        String lastName = scanner.next();
+
+        Matcher match = pattern.matcher(lastName);
+        if (match.matches())
+            System.out.println("Last Name is Valid");
+        else
+            System.out.println("Last Name is InValid");
+    }
 }
